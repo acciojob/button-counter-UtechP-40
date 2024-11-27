22e23ccb6a67500c32dfa4ced2f431c0e1d1ca54
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-11-28 00:45:11 +0530

    Attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value,setValue] = useState(0)
+function increment(){
+setValue(value+1)
+}
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {value} times</p>
+        <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
